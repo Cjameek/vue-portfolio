448448d42c4c975858118229a64a5274433a0744
Cody Meek <codymeek@Codys-MacBook-Pro-2.local>
2020-06-30 23:53:51 -0400

    Commented out JS

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    
   </div>
 </template>
+
+<script>
+export default {
+  name: "About",
+  // data() {
+  //   return {
+  //     content: []
+  //   }
+  // },
+  // methods: {
+  //   getPrismicContent: function() {
+  //     console.log("fire");
+  //   }
+  // },
+  // created: function() {
+  //   //console.log(process.env.PRISMIC_API_URL);
+  // }
+
+  // process.env.PRISMIC_API_URL
+}
+</script>
