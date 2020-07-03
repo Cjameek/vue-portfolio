c7e5623d98d4bb225df27bd8f90a5ab7313498ad
Cody Meek <codymeek@Codys-MacBook-Pro-2.local>
2020-07-03 08:48:51 -0400

    Moved navigation logic into own component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,27 +3,17 @@
     <figure class="header__icon rm">
       <img alt="Vue logo" src="../assets/img/logo.png" />
     </figure>
-    <nav class="header__navigation">
-      <ul class="header__navigation_menu">
-        <li
-          class="header__navigation_menu-item"
-          v-for="item in navItems"
-          :key="item.name"
-        >
-          <a
-            class="header__navigation_menu-item--link"
-            :href="createItemLink(item.name)"
-            >{{ item.name }}</a
-          >
-        </li>
-      </ul>
-    </nav>
+    <Navigation />
   </header>
 </template>
 
 <script>
+import Navigation from "./Navigation";
 export default {
   name: "Header",
+  components: {
+    Navigation
+  },
   data() {
     return {
       navItems: [
