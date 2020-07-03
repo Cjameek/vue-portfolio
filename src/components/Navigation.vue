80982c46e42840acd142fccf7bcacd490a4115e0
Cody Meek <codymeek@Codys-MacBook-Pro-2.local>
2020-07-03 11:06:13 -0400

    Debugging navigation issues

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -3,14 +3,14 @@
     <ul class="header__navigation_menu">
       <li
         class="header__navigation_menu-item"
-        v-for="item in navItems"
+        :for="item in navItems"
         :key="item.name"
       >
-        <a
+        <!-- <a
           class="header__navigation_menu-item--link"
           :href="createItemLink(item.name)"
           >{{ item.name }}</a
-        >
+        > -->
       </li>
     </ul>
   </nav>
