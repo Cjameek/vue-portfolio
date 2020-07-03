ffc18c54993db1d0501775724429b1671e842f0c
Cody Meek <codymeek@Codys-MacBook-Pro-2.local>
2020-07-03 08:48:36 -0400

    Created navigation component

---
+++ src/components/Navigation.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav class="header__navigation">
+    <ul class="header__navigation_menu">
+      <li
+        class="header__navigation_menu-item"
+        v-for="item in navItems"
+        :key="item.name"
+      >
+        <a
+          class="header__navigation_menu-item--link"
+          :href="createItemLink(item.name)"
+          >{{ item.name }}</a
+        >
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navigation",
+  data() {
+    return {
+      navItems: [
+        {
+          name: "About"
+        },
+        {
+          name: "Experience"
+        },
+        {
+          name: "Projects"
+        },
+        {
+          name: "Contact"
+        }
+      ]
+    };
+  },
+  methods: {
+    createItemLink: function(id) {
+      return "#" + id.toLowerCase();
+    }
+  }
+};
+</script>
+
+<style></style>
