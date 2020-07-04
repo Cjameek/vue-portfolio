919eb6ef087f3425e1fb51aa8545f51f2b049c5f
Cody Meek <codymeek@Codys-MacBook-Pro-2.local>
2020-07-03 22:16:37 -0400

    Created hero component

---
+++ src/components/Hero.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hero hero__wrapper">
+    <h1 class="abs-center">{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+  data() {
+    return {
+      msg: "This is a test message"
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+@import "../assets/scss/components/_hero.scss";
+</style>
