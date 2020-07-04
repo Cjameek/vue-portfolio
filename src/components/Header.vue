5d6a60ea3f0d348a0c17b709e81b2dd2e2b9cf7e
Cody Meek <codymeek@Codys-MacBook-Pro-2.local>
2020-07-04 08:55:15 -0400

    Added wrapper around icon & nav

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,19 @@
 <template>
   <header class="header">
-    <figure class="header__icon rm">
-      <img alt="Vue logo" src="../assets/img/logo.png" />
-    </figure>
-    <Navigation />
+    <div class="header__wrapper">
+      <figure class="header__icon rm">
+        <img alt="Vue logo" src="../assets/img/logo.png" />
+      </figure>
+      <Navigation />
+    </div>
+
+    <!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path
+        fill="#FFFFFF"
+        fill-opacity="1"
+        d="M0,128L1440,160L1440,0L0,0Z"
+      ></path>
+    </svg> -->
   </header>
 </template>
 
