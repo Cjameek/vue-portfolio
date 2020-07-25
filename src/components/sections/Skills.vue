8a74a305227001b06aa0a262b6245aefc733558b
Cody Meek <codymeek@Codys-MacBook-Pro-2.local>
2020-07-24 21:16:04 -0400

    Create Skills section component

---
+++ src/components/sections/Skills.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="skills__container">
+    <h2 class="skills__subtitle">This should be fun</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Skills",
+  data() {
+    return {
+      skills: [
+        {
+          name: "Vue"
+        },
+        {
+          name: "Vue"
+        },
+        {
+          name: "Vue"
+        },
+        {
+          name: "Vue"
+        },
+        {
+          name: "Vue"
+        },
+        {
+          name: "Vue"
+        },
+        {
+          name: "Vue"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+.skills {
+  &__container {
+    display: flex;
+    flex-direction: column;
+  }
+}
+</style>
