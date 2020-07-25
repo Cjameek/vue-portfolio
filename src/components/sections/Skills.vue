1ab625a88279adab934d9005d3a8037f11596d0e
Cody Meek <codymeek@Codys-MacBook-Pro-2.local>
2020-07-25 00:01:52 -0400

    Start ring menu layout

--- src/components/sections/Skills.vue
+++ src/components/sections/Skills.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="skills__container">
     <h2 class="skills__subtitle">This should be fun</h2>
+    <ul class="skills__list">
+      <li
+        class="skills__list-item"
+        v-for="skill in skills"
+        :key="createSkillsIcons(skill.name)"
+      >
+        <div class="skills__list-item--inner">
+          <span class="skills__list-item--description">{{ skill.name }}</span>
+          <i
+            :class="
+              `skills__list-item--icon icon-${createSkillsIcons(skill.name)}`
+            "
+            aria-hidden="true"
+          ></i>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -11,28 +28,72 @@ export default {
     return {
       skills: [
         {
-          name: "Vue"
+          name: "Adobe XD"
+        },
+        {
+          name: "Adobe Photoshop"
+        },
+        {
+          name: "Adobe Illustrator"
+        },
+        {
+          name: "mySQL"
+        },
+        {
+          name: "Wordpress"
+        },
+        {
+          name: "PHP"
+        },
+        {
+          name: "React"
         },
         {
           name: "Vue"
         },
         {
-          name: "Vue"
+          name: "JavaScript"
         },
         {
-          name: "Vue"
+          name: "CSS"
         },
         {
-          name: "Vue"
-        },
-        {
-          name: "Vue"
-        },
-        {
-          name: "Vue"
+          name: "HTML5"
         }
       ]
     };
+  },
+  methods: {
+    createSkillsIcons: function(name) {
+      return name.replace(/\s/g, "").toLowerCase();
+    },
+    createRing: function() {
+      const items = document.querySelectorAll(".skills__list-item");
+      items.forEach((item, index) => {
+        return (item.style.transform = `rotate(${(360 / this.skills.length) *
+          index}deg) translate(-250px)`);
+      });
+    },
+    reverseInsideRing: function() {
+      const items = document.querySelectorAll(".skills__list-item");
+      const reverseItems = Object.assign([], items).reverse();
+      reverseItems.forEach((item, index) => {
+        const innerWrap = item.querySelector(".skills__list-item--inner");
+
+        return (innerWrap.style.transform = `rotate(${(360 /
+          this.skills.length) *
+          index}deg)`);
+      });
+    },
+    rotateRing: function() {
+      const ring = document.querySelector(".skills__list");
+      return (ring.style.transform = `rotate(${360 / this.skills.length}deg)`);
+    }
+  },
+  mounted() {
+    this.createRing();
+    this.reverseInsideRing();
+    this.rotateRing();
   }
 };
 </script>
@@ -42,6 +103,71 @@ export default {
   &__container {
     display: flex;
     flex-direction: column;
+  }
+
+  &__list {
+    position: relative;
+    width: 600px;
+    height: 600px;
+
+    &-item {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+
+      &--inner {
+        position: relative;
+        background-color: #fff;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100px;
+        height: 100px;
+        font-size: 50px;
+        text-align: center;
+
+        &:after {
+          content: "";
+          display: block;
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+          transform: scale(1);
+          opacity: 0.6;
+          transition: transform 350ms cubic-bezier(0.175, 0.885, 0.32, 1.275),
+            opacity 350ms cubic-bezier(0.175, 0.885, 0.32, 1.275);
+        }
+
+        &:hover:after {
+          opacity: 1;
+          transform: scale(1.1);
+        }
+      }
+
+      &--description {
+        position: absolute;
+        z-index: -1;
+        width: 1px;
+        height: 1px;
+        visibility: hidden;
+        overflow: hidden;
+        pointer-events: none;
+      }
+
+      // @for $i from 1 through 11 {
+      //   $calculate: (32.73 * $i) + deg;
+      //   $reverse: ((32.73 * $i) - (32.72)) + deg;
+      //   &:nth-child(#{$i}) {
+      //     transform: rotate($calculate) translate(300px);
+      //   }
+      //   &:nth-child(#{$i}) &--inner {
+      //     transform: rotate($reverse);
+      //   }
+      // }
+    }
   }
 }
 </style>
