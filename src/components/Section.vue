4c3deaf35e49b5b96456921c502a6800f9c211e3
Cody Meek <codymeek@Codys-MacBook-Pro-2.local>
2020-07-24 21:16:23 -0400

    Create Section component

---
+++ src/components/Section.vue
@@ -0,0 +1,39 @@
+<template>
+  <section :id="sectionShow">
+    <slot name="sectionTitle">Title</slot>
+    <About v-if="section === 'about'" />
+    <Skills v-if="section === 'skills'" />
+  </section>
+</template>
+
+<script>
+import About from "./sections/About";
+import Skills from "./sections/Skills";
+
+export default {
+  name: "Section",
+  components: {
+    About,
+    Skills
+  },
+  props: {
+    sectionShow: String,
+    contentBody: Object
+  },
+  data() {
+    return {
+      content: this.contentBody,
+      section: this.sectionShow
+    };
+  },
+  computed: {
+    logCurrentContent: function() {
+      return console.log(this.content);
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import "../assets/scss/components/_sections.scss";
+</style>
